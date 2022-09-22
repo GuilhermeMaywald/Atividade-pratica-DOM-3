aaf9a2660a66eaefa479ffb90c19c2ffd0e77a71
guilherme maywald <guilhermemaywald@gmail.com>
2022-09-22 15:59:37 -0300

    Correção 1 validar campos

--- assets/js/dom_3.js
+++ assets/js/dom_3.js
@@ -1,20 +1,14 @@
 
-{
-    let clicouBotao = () => {
-        let nomeUsuario = document.getElementById('nome_usuario');
-        let sobrenomeUsuario = document.getElementById('sobrenome_usuario');
-        let cpfUsuario = document.getElementById('cpf_usuario');
+let clicouBotao = () => {
+    let nomeUsuario = document.getElementById('nome_usuario');
+    let sobrenomeUsuario = document.getElementById('sobrenome_usuario');
+    let cpfUsuario = document.getElementById('cpf_usuario');
 
-        console.log(nomeUsuario);
-        console.log(sobrenomeUsuario);
-        console.log(cpfUsuario);
-
-        // if(nomeUsuario.value == '' || sobrenomeUsuario.value == '' || cpfUsuario.value == '') {
-        //     alert('Favor preencher todos os campos!');
-        //     return false;
-        // } else {
-        //     document.getElementsByTagName('input').submit();
-        //     return true;
-        // }
+    if(nomeUsuario.value == '' || sobrenomeUsuario.value == '' || cpfUsuario.value == '') {
+        alert('Favor preencher todos os campos!');
+        return false;
+    } else {
+        document.getElementsByTagName('input');
+        return true;
     }
 }
