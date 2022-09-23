c7aa5a01021dcb7e1391a9303c777ea878f93005
guilherme maywald <guilhermemaywald@gmail.com>
2022-09-23 09:28:07 -0300

    Resolução final

--- assets/js/dom_3.js
+++ assets/js/dom_3.js
@@ -1,14 +1,31 @@
+let nomeUsuario = document.getElementById('nome_usuario');
+let sobrenomeUsuario = document.getElementById('sobrenome_usuario');
+let cpfUsuario = document.getElementById('cpf_usuario');
+
+
+let recebeCampos = () => {
+    if (nomeUsuario.value == '' || sobrenomeUsuario.value == '' || cpfUsuario.value == '') {
+        console.log('Favor preencher todos os campos!');
+        return false;
+    } 
+}
+
+function validaCpf(cpf){
+    if(cpf.length == 11) {
+        return true;
+    }
+    return false;
+}
 
 let clicouBotao = () => {
-    let nomeUsuario = document.getElementById('nome_usuario');
-    let sobrenomeUsuario = document.getElementById('sobrenome_usuario');
-    let cpfUsuario = document.getElementById('cpf_usuario');
-
-    if(nomeUsuario.value == '' || sobrenomeUsuario.value == '' || cpfUsuario.value == '') {
-        alert('Favor preencher todos os campos!');
-        return false;
+    recebeCampos();
+    let cpfValido = cpfUsuario.value;
+    cpfValido = cpfValido.toString();
+    let cpfConfirmado = validaCpf(cpfValido);
+    if (cpfConfirmado){
+        console.log('CPF Válido!');
+        console.log('Dados enviados com sucesso!')  
     } else {
-        document.getElementsByTagName('input');
-        return true;
+        console.log('CPF Inválido');
     }
 }
